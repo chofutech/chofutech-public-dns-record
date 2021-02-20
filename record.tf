65ff745779a56571ae2c94da9ba8838c576bd4a5
whywaita <whywaita@whywrite.it>
2021-02-21 00:44:24 +0900

    avoid forces replacement

--- record.tf
+++ record.tf
@@ -7,21 +7,21 @@ resource "cloudflare_record" "chofu_tech_CNAME" {
 
 resource "cloudflare_record" "www_chofu_tech_A" {
   zone_id = lookup(data.cloudflare_zones.chofu-tech.zones[0], "id")
-  name = "www.chofu.tech"
+  name = "www"
   value = "151.101.100.133"
   type = "A"
 }
 
 resource "cloudflare_record" "anchor_chofu_tech_A" {
   zone_id = lookup(data.cloudflare_zones.chofu-tech.zones[0], "id")
-  name = "anchor.chofu.tech"
+  name = "anchor"
   value = "103.202.216.76"
   type = "A"
 }
 
 resource "cloudflare_record" "anchor_chofu_tech_AAAA" {
   zone_id = lookup(data.cloudflare_zones.chofu-tech.zones[0], "id")
-  name = "anchor.chofu.tech"
+  name = "anchor"
   value = "2403:bd80:c001:1:103:202:216:76"
   type = "AAAA"
 }
