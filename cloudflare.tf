05f77c1b20ebc624992edbf1e8c1e18031b17523
Tachibana waita <nikoniko.why+github@gmail.com>
2021-02-20 22:33:08 +0900

    for terraform 0.14 (#11)
    * terraform is hard

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,12 +1,22 @@
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.13"
+    }
+  }
+}
+
 variable "cloudflare_email" {
   description = "cloudflare email"
 }
 
-variable "cloudflare_apikey" {
+variable "cloudflare_api_key" {
   description = "cloudflare api key"
 }
 
 provider "cloudflare" {
-  email = "${var.cloudflare_email}"
-  token = "${var.cloudflare_apikey}"
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }
--- record.tf
+++ record.tf
@@ -1,12 +1,12 @@
 resource "cloudflare_record" "chofu_tech_A" {
-    domain = "chofu.tech"
+    zone_id = cloudflare_zone.chofu-tech.id
     name = "chofu.tech"
     value = "151.101.100.133"
     type = "A"
 }
 
 resource "cloudflare_record" "www_chofu_tech_A" {
-    domain = "chofu.tech"
+    zone_id = cloudflare_zone.chofu-tech.id
     name = "www.chofu.tech"
     value = "151.101.100.133"
     type = "A"
