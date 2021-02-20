15238af27d20520765cceebd797c3ec0d1783320
whywaita <whywaita@whywrite.it>
2021-02-20 23:25:41 +0900

    use API Token

--- record.tf
+++ record.tf
@@ -1,12 +1,12 @@
 resource "cloudflare_record" "chofu_tech_A" {
-    zone_id = cloudflare_zone.chofu-tech.id
+    zone_id = lookup(data.cloudflare_zones.chofu-tech.zones[0], "id")
     name = "chofu.tech"
     value = "151.101.100.133"
     type = "A"
 }
 
 resource "cloudflare_record" "www_chofu_tech_A" {
-    zone_id = cloudflare_zone.chofu-tech.id
+    zone_id = lookup(data.cloudflare_zones.chofu-tech.zones[0], "id")
     name = "www.chofu.tech"
     value = "151.101.100.133"
     type = "A"
