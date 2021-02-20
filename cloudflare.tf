e228ac31bb27c10a84f276b9fac3a17efc6f6661
whywaita <whywaita@whywrite.it>
2021-02-20 21:16:39 +0900

    cloudflare provider required terraform 0.12

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,12 +1,3 @@
-variable "cloudflare_email" {
-  description = "cloudflare email"
-}
-
-variable "cloudflare_apikey" {
-  description = "cloudflare api key"
-}
-
 provider "cloudflare" {
-  email = "${var.cloudflare_email}"
-  token = "${var.cloudflare_apikey}"
+  version = "~> 2.0"
 }
