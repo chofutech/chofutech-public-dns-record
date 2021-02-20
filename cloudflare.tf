936d2a52c4647dd932b78d64081d7e9546eccb64
whywaita <whywaita@whywrite.it>
2021-02-20 21:41:07 +0900

    terraform 0.14

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.13"
+    }
+  }
+}
+
 variable "cloudflare_email" {
   description = "cloudflare email"
 }
@@ -7,7 +17,6 @@ variable "cloudflare_api_key" {
 }
 
 provider "cloudflare" {
-  email   = "${var.cloudflare_email}"
-  api_key = "${var.cloudflare_api_key}"
-  version = "~> 2.0"
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }
